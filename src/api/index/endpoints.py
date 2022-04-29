5bd161659077a8300610805f9e06cadfec45437b
mike <mikeryan56@gmail.com>
2022-04-29 19:38:21 +0000

    updating loggin path

--- src/api/index/endpoints.py
+++ src/api/index/endpoints.py
@@ -11,7 +11,7 @@ from resources import templates
 
 async def temp_homepage(request):
 
-    logger.info(request.session)
+    logger.info(f"page '{request.url.path}' accessed")
     return JSONResponse({"message": "Temp Home"}, status_code=200)
 
 
@@ -19,8 +19,9 @@ async def temp_homepage(request):
 async def homepage(request):
     logger.info(request.session)
     # if "user_name" not in request.session:
+    #     logger.info(f"page '{request.url.path}' accessed")
     #     return RedirectResponse(url=f"/", status_code=303)
-
+    logger.info(f"page '{request.url.path}' accessed")
     return RedirectResponse(url=f"/dashboard", status_code=303)
 
 
@@ -29,5 +30,5 @@ async def about_page(request):
 
     template = f"about.html"
     context = {"request": request}
-    logger.info(f"page accessed: /{template}")
+    logger.info(f"page '{request.url.path}' accessed")
     return templates.TemplateResponse(template, context)
