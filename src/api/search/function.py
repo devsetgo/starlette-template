e744002f66373359dd988c805fc6b7d9f0837e75
mike <mikeryan56@gmail.com>
2022-05-29 19:28:30 +0000

    limit to 20

--- src/api/search/function.py
+++ src/api/search/function.py
@@ -2,7 +2,8 @@
 import silly
 
 
-def search_creator(qty: int = 50) -> list:
+# this should be replaced with a function that does actual searches
+async def search_creator(qty: int = 20) -> list:
     search_data: list = []
     for i in range(qty):
         data: dict = {
