a05a8e5545612a2c8ed15435ad525b495deac77d
mike <mikeryan56@gmail.com>
2022-05-29 03:11:16 +0000

    Search and use silly to generate random text

---
+++ src/api/search/endpoints.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from loguru import logger
+from sqlalchemy.sql.functions import session_user
+from starlette.exceptions import HTTPException
+from starlette.responses import JSONResponse, RedirectResponse
+
+# from core import login_required
+from resources import templates
+from api.search.function import search_creator
+
+# @login_required.require_login
+
+
+async def search_page(request):
+    # html_page = request.path_params["page"]
+    try:
+        template = "/search/index.html"
+
+        context = {"request": request, "data": search_creator()}
+        logger.info(f"page accessed: {template}")
+        return templates.TemplateResponse(template, context)
+    except Exception as e:
+        detail:str = f"Error: Page accessed: /search/index , but HTML page {e} does not exist"
+        logger.error(detail)
+        raise HTTPException(404, detail=detail)
---
+++ src/api/search/function.py
@@ -0,0 +1,15 @@
+import silly
+
+
+def search_creator(qty: int = 50) -> list:
+    search_data: list = []
+    for i in range(qty):
+        data: dict={
+            "name": silly.name(),
+            "thing": silly.thing(),
+            "domain": silly.domain(),
+            "text": silly.sentence(),
+            "email": silly.email(),
+        }
+        search_data.append(data)
+    return search_data
